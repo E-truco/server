149df413039f089d1c67586364f9315bda205073
codykoinabox <codykoinabox@gmail.com>
2024-05-21 21:20:22 -0300

    create game component (not finished)

---
+++ components/Game.js
@@ -0,0 +1,18 @@
+
+
+class Game{
+
+    constructor(){
+
+        this.currentRound = 0;
+        this.roundHistory = [];
+
+    }
+
+    // this method will be done last, after all the other components are done
+    gameHandler(){
+
+    }
+}
+
+export default Game
