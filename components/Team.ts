5b4a4aad7cea5e2bf4f3e3fee3583718ff134941
codykoinabox <codykoinabox@gmail.com>
2024-05-28 19:42:26 -0300

    update team class from JS to typescript

--- components/Team.ts
+++ components/Team.ts
@@ -3,10 +3,13 @@ import Player from './Player'
 class Team{
 
     teamID : string;
-    players : Player[]
+    players : Player[];
+    playPoints : number;
+    roundPoints : number;
 
-    constructor(teamID){
-        this.teamID = teamID
+    constructor(teamID : string){
+
+        this.teamID = teamID;
         this.players = [];
         this.playPoints = 0;
         this.roundPoints = 0;
@@ -14,7 +17,7 @@ class Team{
     }
 
     // this will receive a Player object and add it to the players[] list
-    addPlayer(player){
+    addPlayer(player : Player){
 
         try{
 
