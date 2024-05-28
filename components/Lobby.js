884f25e5285f406f9660e5c91c42555c831e9c58
codykoinabox <codykoinabox@gmail.com>
2024-05-28 19:26:33 -0300

    changes to lobby class

--- components/Lobby.js
+++ components/Lobby.js
@@ -1,9 +1,27 @@
 
 class Lobby{
 
-    // all players in a lobby (should of type Player)
-    constructor(players){
-        this.players = players;
+    
+    constructor(lobbyID, lobbyName){
+        this.lobbyID = lobbyID;
+        this.lobbyName = lobbyName;
+        this.players = [];
+    }
+
+    // will add a player object to the lobby
+    addPlayer(player){
+
+        try{
+
+            this.players.push(player)
+
+        }catch{
+
+            return `ERROR. PLAYER NOT ADDED TO LOBBY ${this.lobbyName}`
+
+        }
+
+        return `PLAYER ADDED TO LOBBY ${this.lobbyName}`
     }
 }
 
