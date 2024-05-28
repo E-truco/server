585412893d7d34f575fece127d1aea375c210588
codykoinabox <codykoinabox@gmail.com>
2024-05-28 19:40:37 -0300

    update player class from JS to typescript

--- components/Player.ts
+++ components/Player.ts
@@ -2,23 +2,28 @@ import Card from './Card'
 
 class Player{
 
-    hand : Card[]
+    hand : Card[];
+    playerID : string;
+    nickname : string;
 
-    constructor(playerID, nickname){
+    constructor(playerID : string, nickname : string){
         this.hand = []
         this.playerID = playerID
-        this.nickname = nickname;
+        this.nickname = nickname
     }
 
+    // will be done later
     play(){
 
     }
 
+    // will be done later
     truco(){
 
 
     }
 
+    // will be done later
     answerTruco(){
 
         
